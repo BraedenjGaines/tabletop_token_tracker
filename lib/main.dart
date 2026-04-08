d15d43d63b5e2c5a2b0af2c190c6e587068278b3
Braeden <braedenjgaines@gmail.com>
2026-04-08 18:16:29 -0500

    Building classes for the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import "package:flutter/material.dart";
+
 void main() {
-  runApp(MaterialApp(
-    title: 'TableTop Token Tracker',
-    home: Scaffold(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TableTop Token Tracker',
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Resource Tracker'),
       ),
       body: Center(
         child: Text('You have one million health!'),
       ),
-    ),
-  ));
+    );
+  }
 }
